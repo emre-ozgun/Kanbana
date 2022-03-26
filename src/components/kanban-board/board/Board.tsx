e26a7a329ffbf6e308dbc15b02839003911ac66c
emre-ozgun <emre2996ozgun@gmail.com>
2022-03-26 22:01:27 +0300

    (add/ui) kanbanboard main layout

--- src/components/kanban-board/board/Board.tsx
+++ src/components/kanban-board/board/Board.tsx
@@ -1,3 +1,5 @@
+import ListComposer from './ListComposer';
+
 import './Board.css';
 
 type ListType = {
@@ -9,8 +11,8 @@ const Board = ({ lists }: ListType) => {
 
 	return (
 		<main className='board'>
-			<div className='board-wrapper'>
-				<div className='list-container'>
+			<div className='list-wrapper'>
+				<div className='list-content'>
 					<div className='list-header'>list 1</div>
 					<div className='card-container'>
 						<div className='card'>card 1</div>
@@ -22,8 +24,11 @@ const Board = ({ lists }: ListType) => {
 						<div className='card'>card 7</div>
 						<div className='card'>card 8</div>
 					</div>
+					<div className='card-composer'>+ Add new card</div>
 				</div>
-				<div className='list-container'>
+			</div>
+			<div className='list-wrapper'>
+				<div className='list-content'>
 					<div className='list-header'>list 1</div>
 					<div className='card-container'>
 						<div className='card'>card 1</div>
@@ -35,17 +40,108 @@ const Board = ({ lists }: ListType) => {
 						<div className='card'>card 7</div>
 						<div className='card'>card 8</div>
 					</div>
+					<div className='card-composer'>+ Add new card</div>
 				</div>
-				<div className='list-container'>
+			</div>
+			<div className='list-wrapper'>
+				<div className='list-content'>
 					<div className='list-header'>list 1</div>
 					<div className='card-container'>
 						<div className='card'>card 1</div>
 						<div className='card'>card 2</div>
 						<div className='card'>card 3</div>
 						<div className='card'>card 4</div>
+						<div className='card'>card 5</div>
+						<div className='card'>card 6</div>
+						<div className='card'>card 7</div>
+						<div className='card'>card 8</div>
 					</div>
+					<div className='card-composer'>+ Add new card</div>
 				</div>
-				<div className='list-composer'>+ Add new list</div>
+			</div>
+			<div className='list-wrapper'>
+				<div className='list-content'>
+					<div className='list-header'>list 1</div>
+					<div className='card-container'>
+						<div className='card'>card 1</div>
+						<div className='card'>card 2</div>
+						<div className='card'>card 3</div>
+						<div className='card'>card 4</div>
+						<div className='card'>card 5</div>
+						<div className='card'>card 6</div>
+						<div className='card'>card 7</div>
+						<div className='card'>card 8</div>
+						<div className='card'>card 1</div>
+						<div className='card'>card 2</div>
+						<div className='card'>card 3</div>
+						<div className='card'>card 4</div>
+						<div className='card'>card 5</div>
+						<div className='card'>card 6</div>
+						<div className='card'>card 7</div>
+						<div className='card'>card 8</div>
+						<div className='card'>card 1</div>
+						<div className='card'>card 2</div>
+						<div className='card'>card 3</div>
+						<div className='card'>card 4</div>
+						<div className='card'>card 5</div>
+						<div className='card'>card 6</div>
+						<div className='card'>card 7</div>
+						<div className='card'>card 8</div>
+						<div className='card'>card 1</div>
+						<div className='card'>card 2</div>
+						<div className='card'>card 3</div>
+						<div className='card'>card 4</div>
+						<div className='card'>card 5</div>
+						<div className='card'>card 6</div>
+						<div className='card'>card 7</div>
+						<div className='card'>card 8</div>
+						<div className='card'>card 1</div>
+						<div className='card'>card 2</div>
+						<div className='card'>card 3</div>
+						<div className='card'>card 4</div>
+						<div className='card'>card 5</div>
+						<div className='card'>card 6</div>
+						<div className='card'>card 7</div>
+						<div className='card'>card 8</div>
+					</div>
+					<div className='card-composer'>+ Add new card</div>
+				</div>
+			</div>
+			<div className='list-wrapper'>
+				<div className='list-content'>
+					<div className='list-header'>list 1</div>
+					<div className='card-container'>
+						<div className='card'>card 1</div>
+						<div className='card'>card 2</div>
+						<div className='card'>card 3</div>
+						<div className='card'>card 4</div>
+						<div className='card'>card 5</div>
+						<div className='card'>card 6</div>
+						<div className='card'>card 7</div>
+						<div className='card'>card 8</div>
+					</div>
+					<div className='card-composer'>+ Add new card</div>
+				</div>
+			</div>
+			<div className='list-wrapper'>
+				<div className='list-content'>
+					<div className='list-header'>list 1</div>
+					<div className='card-container'>
+						<div className='card'>card 1</div>
+						<div className='card'>card 2</div>
+						<div className='card'>card 3</div>
+						<div className='card'>card 4</div>
+						<div className='card'>card 5</div>
+						<div className='card'>card 6</div>
+						<div className='card'>card 7</div>
+						<div className='card'>card 8</div>
+					</div>
+					<div className='card-composer'>+ Add a card</div>
+				</div>
+			</div>
+
+			<div className='list-wrapper'>
+				<ListComposer />
 			</div>
 		</main>
 	);
---
+++ src/components/kanban-board/board/ListComposer.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useRef, useEffect } from 'react';
+import useOnClickOutside from '../../../utils/useClickOutside';
+import './ListComposer.css';
+
+const ListComposer = () => {
+	const [isListComposerOpen, setIsListComposerOpen] = useState(false);
+
+	const listComposerRef = useRef<null | HTMLDivElement>(null);
+	const listComposerInputRef = useRef<null | HTMLInputElement>(null);
+
+	useOnClickOutside(listComposerRef, () => setIsListComposerOpen(false));
+
+	useEffect(() => {
+		if (isListComposerOpen) {
+			if (listComposerInputRef && listComposerRef.current) {
+				listComposerInputRef?.current?.focus();
+			}
+		}
+	}, [isListComposerOpen]);
+
+	if (isListComposerOpen) {
+		return (
+			<div
+				className={`list-composer ${isListComposerOpen && 'active'}`}
+				ref={listComposerRef}
+			>
+				<form className='list-composer__form'>
+					<input
+						ref={listComposerInputRef}
+						type='text'
+						placeholder='Enter list title...'
+						className='list-composer__form-input'
+					/>
+					.
+					<div className='list-composer__cta'>
+						<button className='list-composer__cta-btn'>Add List</button>
+						<button
+							onClick={() => setIsListComposerOpen(false)}
+							className='list-composer__cta-btn-close'
+						>
+							X
+						</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
+
+	return (
+		<div className='list-composer' onClick={() => setIsListComposerOpen(true)}>
+			+ Add new list
+		</div>
+	);
+};
+
+export default ListComposer;
