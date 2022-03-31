bee174e86d448024e53be4e8e2b23637ae64ce2e
Emre Oz <50968552+emre-ozgun@users.noreply.github.com>
2022-03-31 19:28:55 +0300

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,7 @@ import boardListReducer from './features/boardList/boardSlice';
 import boardReducer from './features/board/kanbanSlice';
 import userListReducer from './features/userList/userListSlice';
 import cardReducer from './features/cardEdit/cardSlice';
-// ...
+
 
 export const store = configureStore({
 	reducer: {
@@ -16,7 +16,7 @@ export const store = configureStore({
 	},
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export type AppDispatch = typeof store.dispatch;
